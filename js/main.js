a43e1c1f57c17390246f03241e1ba7481efcb124
HenrikTk <henrik.ruan4@gmail.com>
2022-02-21 09:04:17 -0300

    test 21-02

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu')
 
 hamburger.addEventListener('click', function (){
     this.classList.toggle('is-active')
+    menu.classList.toggle('menu-is-active')
 })
