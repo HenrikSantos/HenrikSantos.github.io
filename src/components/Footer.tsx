b8a9bf503fc7ec6edb8aedffcae685f83ec97ed7
HenrikSantos <henrik.ruan4@gmail.com>
2024-05-15 21:21:34 -0300

    💬 fix description errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
-
 export default function Footer() {
   return (
     <footer className='primary'>
       <h1 className='text-center'>
-          © 2023 Copyright: <span>Henrik Ruan Santos de Souza</span>
+          © 2024 Copyright: <span>Henrik Ruan Santos de Souza</span>
       </h1>
     </footer>
   );
