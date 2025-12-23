820f53d6b06c80c93374901cf17d9343e94acbdf
HenrikSantos <henrik.ruan4@gmail.com>
2025-12-23 13:10:54 -0300

    feat: update Stack with new technologies

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -2,29 +2,36 @@ export default function Stack() {
   return (
     <section id="stack" className="flex flex-col justify-center">
       <h1 className="mb-1">
-        <span className="text-4xl font-semibold">Tech Stack:</span>
+        <span className="text-4xl font-semibold">Tecnologias:</span>
       </h1>
-      <ul className="mx-3 flex flex-col space-y-1 font-thin">
+      <ul className="ml-3 flex flex-col space-y-1 font-thin">
         <li>
-          - <span className="text-lg underline">Front-end</span>: HTML, CSS,
-          Javascript, React, Redux, Tailwind e Bootstrap;
+          - <span className="text-lg underline">Backend</span>: Node.js, NestJS,
+          Express, JWT/AUTH, APIs REST, WebSocket, SSE, Cron Jobs;
         </li>
         <li>
-          - <span className="text-lg underline">Back-end</span>: Firebase, GCP,
-          TypeScript, Cloud functions, Python, Node, Express, MySql, MongoDB,
-          Mongoose e Sequelize;
+          - <span className="text-lg underline">Frontend</span>: React,
+          TypeScript, Tailwind CSS;
         </li>
         <li>
-          - <span className="text-lg underline">QA</span>: Jest, Vitest, Mocha,
-          Chai, Sinon e Pytest;
+          - <span className="text-lg underline">Databases</span>: Postgres,
+          MySQL, MariaDB, MongoDB, Firestore, Prisma, TypeORM;
         </li>
         <li>
-          - <span className="text-lg underline">Arquiteturas</span>: Cloud
-          computing, RESTful, TDD e SOLID;
+          - <span className="text-lg underline">Cloud Computing</span>: GCP
+          (Firebase, Cloud Storage, Cloud Functions), AWS (EC2, S3, RDS);
         </li>
         <li>
-          - <span className="text-lg underline">Outros</span>: Git, Docker,
-          Mermaid, Pubsub e Eslint;
+          - <span className="text-lg underline">Ferramentas</span>: Git, GitHub,
+          Swagger, Postman, Docker;
+        </li>
+        <li>
+          - <span className="text-lg underline">Integrações</span>: Gateways de
+          pagamento (Stripe, Pagar.me, Asaas), e-mail (SendGrid, Brevo);
+        </li>
+        <li>
+          - <span className="text-lg underline">Boas Práticas</span>: SOLID,
+          Clean Code, Arquitetura de Software;
         </li>
       </ul>
     </section>
